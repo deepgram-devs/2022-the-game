559e01b7ff17eb92c36a24aab846c63592e9db55
jgberry <jgbcode@gmail.com>
2022-11-16 17:24:49 -0500

    Serve frontend

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,12 +1,18 @@
-from flask import Flask
+import flask
+import os
 from . import config
 
-app = Flask(__name__)
+app = flask.Flask(__name__, static_folder="/build")
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return flask.send_from_directory(app.static_folder, path)
+    else:
+        return flask.send_from_directory(app.static_folder, 'index.html')
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
