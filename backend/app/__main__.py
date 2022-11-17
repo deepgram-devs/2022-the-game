b6b528282d68b86bf88b6c90232c1733583233ba
jgberry <jgbcode@gmail.com>
2022-11-17 16:15:11 -0500

    Formatting

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,6 +1,9 @@
+import os
+
 import flask
 import flask_sock
-import os
+import simple_websocket
+
 from . import config
 
 app = flask.Flask(__name__, static_folder="/build")
@@ -13,8 +16,8 @@ def serve_root():
     return flask.redirect(flask.url_for("serve_app"))
 
 
-@app.route('/app', defaults={'path': ''})
-@app.route('/app/<path:path>')
+@app.route("/app", defaults={"path": ""})
+@app.route("/app/<path:path>")
 def serve_app(path: str) -> flask.Response:
     if path == "":
         return flask.send_from_directory(app.static_folder, "index.html")
@@ -25,8 +28,8 @@ def serve_app(path: str) -> flask.Response:
 
 # WebSocket endpoint
 @sock.route("/play")
-def play(ws):
+def play(ws: simple_websocket.Server) -> None:
     pass
 
 
-app.run(host='0.0.0.0', port=config.APP_PORT, debug=config.APP_DEBUG)
+app.run(host="0.0.0.0", port=config.APP_PORT, debug=config.APP_DEBUG)
